1c8d3773f3637c1b358d4834287a7427ddbbf371
nklapste <nklapste@ualberta.ca>
2018-10-03 17:46:54 -0600

    updating docs

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetSerializer.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetSerializer.java
@@ -16,20 +16,42 @@ import java.lang.reflect.Type;
  * Add-in to allow GSON/JSON Serializing of the abstract class Tweet and its subclasses.
  *
  * @see LonelyTwitterPreferencesManager uses this to properly (de)serialize {@code TweetList}s.
- * @see com.google.gson.JsonDeserializer
- * @see com.google.gson.JsonSerializer
  */
 class TweetSerializer implements JsonSerializer<Tweet>, JsonDeserializer<Tweet> {
     private static final String TAG = "TweetSerializer";
 
+    /**
+     * Serialize an {@code Tweet} and consider additional class attributes such as type and properties.
+     *
+     * @see Tweet
+     * @see com.google.gson.JsonSerializer
+     *
+     * @param tweet {@code Tweet}
+     * @param typeOfSrc {@code Type}
+     * @param context {@code JsonSerializationContext}
+     * @return {@code JsonElement}
+     */
     @Override
-    public JsonElement serialize(Tweet src, Type typeOfSrc, JsonSerializationContext context) {
+    public JsonElement serialize(Tweet tweet, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject result = new JsonObject();
-        result.add("type", new JsonPrimitive(src.getClass().getSimpleName()));
-        result.add("properties", context.serialize(src, src.getClass()));
+        result.add("type", new JsonPrimitive(tweet.getClass().getSimpleName()));
+        result.add("properties", context.serialize(tweet, tweet.getClass()));
         return result;
     }
 
+    /**
+     * Deserialize a JsonElement considering additional class attributes such as
+     * type and properties.
+     *
+     * @see Tweet
+     * @see com.google.gson.JsonDeserializer
+     *
+     * @param json {@code JsonElement}
+     * @param typeOfT {@code Type}
+     * @param context {@code JsonDeserializationContext}
+     * @return {@code Tweet} or a respective subclass of {@code Tweet}.
+     * @throws JsonParseException if the class specified by the deserializer does not exist or is unknown.
+     */
     @Override
     public Tweet deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
             throws JsonParseException {
