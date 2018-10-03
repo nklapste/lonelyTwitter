64e0493c3450cd5eb5d11a9962a32eafbe907ed8
nklapste <nklapste@ualberta.ca>
2018-10-03 17:01:14 -0600

    improving class doc
    refactoring

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetAdapter.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetAdapter.java
@@ -7,10 +7,9 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 
 /**
- * Tweet adapter for integrating a {@code TweetList}.
+ * {@code ArrayAdapter} for integrating a {@code TweetList}.
  */
 class TweetAdapter extends ArrayAdapter<Tweet> {
     private static final String TAG = "TweetAdapter";
@@ -21,7 +20,7 @@ class TweetAdapter extends ArrayAdapter<Tweet> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
+        // Get the Tweet item for this position
         Tweet tweet = getItem(position);
 
         // Check if an existing view is being reused, otherwise inflate the view
@@ -29,7 +28,7 @@ class TweetAdapter extends ArrayAdapter<Tweet> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
 
-        // set the message of the tweet
+        // set the message of the Tweet
         TextView tweetMessage = (TextView) convertView.findViewById(R.id.tweet_message);
         tweetMessage.setText(tweet.getMessage());
         return convertView;
