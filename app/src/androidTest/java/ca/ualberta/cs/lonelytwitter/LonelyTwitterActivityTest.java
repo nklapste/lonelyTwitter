83e6b83852b3a6d3f0de618e3fc19a1e270985ef
nklapste <nklapste@ualberta.ca>
2018-10-17 14:17:49 -0600

    updating test structure

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,14 +1,18 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.rule.ActivityTestRule;
 
 import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by wz on 14/09/15.
  */
-public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+public class LonelyTwitterActivityTest extends ActivityTestRule<LonelyTwitterActivity> {
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
@@ -16,6 +20,15 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
 
     public void testStart() throws Exception {
         Activity activity = getActivity();
+    }
 
+    @Test
+    public void testFooSuccess() {
+        assertEquals("1","1");
+    }
+
+    @Test
+    public void testFooFail() {
+        assertEquals("1","2");
     }
 }
---
+++ app/src/test/java/ca/ualberta/cs/lonelytwitter/TestTest.java
@@ -0,0 +1,21 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by wz on 14/09/15.
+ */
+public class TestTest {
+
+    @Test
+    public void testFooSuccess() {
+        assertEquals("1","1");
+    }
+
+    @Test
+    public void testFooFail() {
+        assertEquals("1","2");
+    }
+}
