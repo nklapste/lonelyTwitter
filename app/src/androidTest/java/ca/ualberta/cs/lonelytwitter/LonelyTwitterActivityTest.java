a517dafc962bd6acbe455057a842562132c31421
nklapste <nklapste@ualberta.ca>
2018-10-17 17:37:08 -0600

    cleaning test

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,34 +1,21 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import android.app.Activity;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+@RunWith(AndroidJUnit4.class)
+public class LonelyTwitterActivityTest {
+    @Rule
+    public ActivityTestRule<LonelyTwitterActivity> activityActivityTestRule = new ActivityTestRule<LonelyTwitterActivity>(LonelyTwitterActivity.class);
+    private LonelyTwitterActivity mLonelyTwitterActivity;
 
-/**
- * Created by wz on 14/09/15.
- */
-public class LonelyTwitterActivityTest extends ActivityTestRule<LonelyTwitterActivity> {
-
-    public LonelyTwitterActivityTest() {
-        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
-    }
-
-    public void testStart() throws Exception {
-        Activity activity = getActivity();
-    }
-
-    @Test
-    public void testFooSuccess() {
-        assertEquals("1","1");
-    }
-
-    @Test
-    public void testFooFail() {
-        assertEquals("1","2");
+    @Before
+    public void init(){
+        mLonelyTwitterActivity = activityActivityTestRule.getActivity();
     }
 }
