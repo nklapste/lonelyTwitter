722aefc97cde8fe836bd554e9813297530634d02
nklapste <nklapste@ualberta.ca>
2018-10-24 18:07:16 -0600

    adding new testing

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,21 +1,53 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import android.app.Activity;
-import android.test.ActivityInstrumentationTestCase2;
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.EditText;
 
-import junit.framework.TestCase;
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.*;
 
 /**
  * Created by wz on 14/09/15.
  */
-public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+@RunWith(AndroidJUnit4.class)
+public class LonelyTwitterActivityTest extends ActivityTestRule<LonelyTwitterActivity> {
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
     }
 
-    public void testStart() throws Exception {
-        Activity activity = getActivity();
+    private Solo solo;
 
+    @Before
+    public void Setup(){
+        super.launchActivity(new Intent());
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @After
+    public void tearDown(){
+        solo.finishOpenedActivities();
+    }
+
+    @Test
+    public void testTweet(){
+        solo.assertCurrentActivity("wrong activity", "LonelyTwitterActivity");
+    }
+
+    @Test
+    public void testAddTweet(){
+        solo.enterText((EditText) solo.getView(R.id.body), "new thing");
+        solo.clickOnButton("Save");
+        solo.clearEditText((EditText) solo.getView(R.id.body));
+        assertTrue(solo.waitForText("new thing"));
     }
 }
